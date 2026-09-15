f953c1f23d67e0ee0998eed321db077c7eac59cb
dacviet.nguyen <dacviet.nguyen@bupa.com.au>
2026-09-15 23:50:10 +1000

    fix

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/barcode/",
   plugins: [react()],
-  base: "/barcode/", // Add this - GitHub Pages requires this for repo-based sites
-  server: {
-    host: true,
-    proxy: {
-      "/api": {
-        target: process.env.VITE_API_URL || "http://localhost:4000",
-        changeOrigin: true,
-      },
-    },
-  },
 });
